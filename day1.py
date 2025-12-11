97334fdbd99e2cd55a180cf98ba4e7e8e98c4e2c
v0idheaven <dahiyavarun2007@gmail.com>
2025-12-12 00:29:46 +0530

    Removed duplicate day1.py and day2.py files

--- day1.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the brand name generator.")
-city = input("Which city did you grow up in?\n")
-pet = input("What is the name of a pet?\n")
-print("Your brand name could be " + city + " " + pet)
