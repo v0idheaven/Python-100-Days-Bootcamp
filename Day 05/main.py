24468b2bee4c06a2cf0b20671f414ff218d60f5c
v0idheaven <dahiyavarun2007@gmail.com>
2025-12-17 23:41:49 +0530

    Day 05 complete

--- Day 05/main.py
+++ Day 05/main.py
@@ -23,6 +23,13 @@ for i in range(0,101):
         sum2 += i
 print(sum2)
 
+sum3 = 0
+for i in range(0,101):
+    if i%3 == 0:
+        continue
+    else:
+        print(i)
+
 
 # FIZZ BUZZ GAME
 for number in range(0,101):
